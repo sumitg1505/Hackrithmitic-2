fb1aec56fdeb38b49cbfbd81fe2344eb1388b1f5
Sumit Gupta <122437569+sumitg1505@users.noreply.github.com>
2023-01-29 20:09:45 +0530

    Add assets

--- web_app/src/App.jsx
+++ web_app/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import WebRoutes from './routes/WebRoutes'
 
 export default function App() {
   return (
-    <div className='font-josefinSans bg-red-500'>Hello World</div>
+    <div className='font-josefinSans'><WebRoutes /></div>
   )
 }
